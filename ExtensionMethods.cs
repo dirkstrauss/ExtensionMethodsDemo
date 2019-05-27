58485e29cad2b23e49692b3691e24d7352f2f2c4
Dirk Strauss <dirk.strauss@thisisembrace.com>
2019-05-27 23:46:38 +0200

    A small sample project to illustrate the use of extension methods

---
+++ ExtensionMethods.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethodDemo
+{
+    public static class ExtensionMethods
+    {
+        public static bool ToInt(this string value)
+        {
+            return int.TryParse(value, out var _);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethodDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sValue = "15A";
+            if (sValue.ToInt())
+            {
+                Console.WriteLine("Valid Integer");
+            }
+            else
+            {
+                Console.WriteLine("Not a valid Integer");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
